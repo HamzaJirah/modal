e659701fbad998b6adfba96f8d74373f010fe126
HamzaJirah <hamzajirah@gmail.com>
2018-10-17 04:13:58 +0100

    changed variable name

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 //select modal element
 const modal = document.querySelector('#simpleModal');
 //select open modal button
-const button = document.querySelector('#modalBtn');
+const modalBtn = document.querySelector('#modalBtn');
 //select close button
 const closeBtn = document.querySelector('#closeBtn');
 
 //listen for click event
+
