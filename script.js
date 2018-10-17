7d0744b84740ea294ab993cc669f39387c467d87
HamzaJirah <hamzajirah@gmail.com>
2018-10-17 04:10:56 +0100

    added comment about close btn

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //select modal element
 const modal = document.querySelector('#simpleModal');
-
 //select open modal button
 const button = document.querySelector('#modalBtn');
+//select close button
