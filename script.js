423805dc374e88f1c1b65f6ca893a782b9363357
HamzaJirah <hamzajirah@gmail.com>
2018-10-17 04:11:53 +0100

    selected close btn element

--- script.js
+++ script.js
@@ -3,3 +3,4 @@ const modal = document.querySelector('#simpleModal');
 //select open modal button
 const button = document.querySelector('#modalBtn');
 //select close button
+const closeBtn = document.querySelector('#closeBtn');
